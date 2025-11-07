d5d690e4ac7bd9c8e2e10b1a79e3f575bf3b8c5f
ExgleGaming <EaglesFan91x@outlook.com>
2025-11-06 23:35:47 -0500

    writing code to control the player

--- Scripts/Entities/Player/Player.cs
+++ Scripts/Entities/Player/Player.cs
@@ -8,7 +8,15 @@ public partial class Player : CharacterBody2D
     private const float Acceleration = 15f;
     
     private Vector2 _inputDirection = Vector2.Zero;
+    private string _facingDirection = "Down";
+    private string _animationToPlay;
+    private AnimatedSprite2D _animatedSprite2D;
 
+    public override void _Ready()
+    {
+        _animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+    }
+    
     public override void _Process(double delta)
     {
         _inputDirection = Input.GetVector("moveLeft", "moveRight", "moveUp", "moveDown");
@@ -16,5 +24,50 @@ public partial class Player : CharacterBody2D
         Velocity = Velocity.Lerp(_inputDirection * Speed, Acceleration * (float)delta);
 
         MoveAndSlide();
+        AnimationToPlay();
+    }
+
+    private string GetDirectionName()
+    {
+        if (_inputDirection == Vector2.Zero)
+        {
+            return _facingDirection;
+        }
+
+        if (_inputDirection.Y > 0)
+        {
+            _facingDirection = "Down";
+        }
+        else if (_inputDirection.Y < 0)
+        {
+            _facingDirection = "Up";
+        }
+        else
+        {
+            if (_inputDirection.X > 0)
+            {
+                _facingDirection = "Right";
+            }
+            else if (_inputDirection.X < 0)
+            {
+                _facingDirection = "Left";
+            }
+        }
+        
+        return _facingDirection;
+    }
+
+    private void AnimationToPlay()
+    {
+        if (Velocity.Length() > 20f)
+        {
+            _animationToPlay = "Run_" + GetDirectionName();
+        }
+        else
+        {
+            _animationToPlay = "Idle_" + GetDirectionName();
+        }
+        
+        _animatedSprite2D.Play(_animationToPlay);
     }
 }
